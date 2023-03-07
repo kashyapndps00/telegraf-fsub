ef3231b402f73f23c1124a091244caf7beec61ca
kashyapndps00 <kashyapsharmandps@gmail.com>
2023-03-07 22:44:17 +0530

    docs: Added README

---
+++ README.md
@@ -0,0 +1,45 @@
+# Telegraf Fsub
+This plugin can help you to force your bot users to subscribe your channel before using the bot.
+
+### Installation
+You can install this plugin any Telegraf Bot.
+
+Install the NPM Plugin
+```bash
+npm i telegraf-fsub
+#or
+yarn add telegraf-fsub
+```
+
+Import and Use in code
+
+```TS (Node)
+import fsub from "telegraf-fsub"
+
+const botFsub = fsub({ 
+    channels : ["@channel"]
+});
+bot.use(await botFsub)
+```
+
+### Advanced Installation
+You can pass these options in bot like this
+```TS (Node)
+import fsub from "telegraf-fsub"
+
+const botFsub = fsub({ 
+    channels: [],
+    notJoinedMessage: "Hello {user}, You Must Joined <b>{channel}</b>",
+    enable_inline: true,
+    parse_mode: "HTML"
+});
+bot.use(await botFsub)
+```
+
+**Note** : channels option is required without that bot will crash
+
+### Help
+You can open a **Issue** for ressolving your problem
+
+### Contribution
+You can contribute to this plugin by just opening a pull request.
---
+++ index.ts
@@ -0,0 +1,45 @@
+import { Middleware, Context, Markup } from "telegraf"
+export interface initial_options {
+    channels: string[] | number[] | [],
+    notJoinedMessage?: string | "Hello {user}, You Must Joined {channel}",
+    enable_inline?: boolean,
+    parse_mode?: "Markdown" | "HTML"
+}
+export default async function fsub(opts: initial_options): Promise<Middleware<Context>> {
+    var { channels, notJoinedMessage, enable_inline, parse_mode } = opts;
+    if (!enable_inline) enable_inline = false;
+    if (!channels) throw new Error("FsubErr: Parameter Check Channels Not Provided");
+    if (!parse_mode) parse_mode = "Markdown";
+
+    const middleware = async (ctx: Context, next: () => Promise<void>) => {
+        if (!ctx.from) throw Error("FsubErr: ctx.from is undefined");
+        for (var channel of channels) {
+            var result = await ctx.telegram.getChatMember(channel, ctx.from.id).catch((e:Error)=>{
+                throw new Error(`FsubError: Cannot Check For Chat 2${channel} | Error ~ ${e.message}`)
+            });
+            var { status } = result;
+            if (!(status == "member" || status == "administrator" || status == "creator")) {
+                if (!notJoinedMessage) {
+                    notJoinedMessage = "Hello {user}, You Must Join {channel}";
+                }
+                var replyStr = notJoinedMessage.replace("{user}", ctx.from.first_name);
+                var replyStr = notJoinedMessage.replace("{user_id}", ctx.from.id.toString());
+                var replyStr = notJoinedMessage.replace("{channel}", `@${channel}`);
+
+                if(enable_inline){
+                    Markup.inlineKeyboard([
+                        Markup.button.url("Join Channel",`https://t.me/${channel}`)
+                    ]);
+                }
+                ctx.reply(replyStr, { parse_mode });
+                break;
+                return;
+            }
+            else{
+                continue;
+            }
+        }
+        await next();
+    }
+    return middleware;
+}
